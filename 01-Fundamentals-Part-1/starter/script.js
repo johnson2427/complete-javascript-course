5177e1befb459ae12c47517b0754d4b1a50ad312
johnson2427 <blakeejohnson39@gmail.com>
2020-11-28 14:15:27 -0600

    break from course midway through first chapter and added script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+
+let country = "USA";
+let continent = "North America";
+let population = 330;
+
+console.log(country)
+console.log(population)
+console.log(continent)
+
+
+// let const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+*/
+const ageJonas = 2037 - 1991;
+console.log(ageJonas);
